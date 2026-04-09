81ac1daa3d313f63640690b6001e6c5ca66a0a0c
julio-m-oliveira_bradesco <julio.m.oliveira@bradesco.com.br>
2026-04-09 20:59:34 -0300

    Atividade switch

--- logica/src/main/java/org/example/Main.java
+++ logica/src/main/java/org/example/Main.java
@@ -1,34 +1,69 @@
-package com.example.demo;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package org.example;
 
 import java.util.Scanner;
 
-@SpringBootApplication
-public class DemoApplication {
+public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o nome do Aluno:");
-        String nomeAluno = scanner.nextLine();
+        int opcao = 0;
 
-        System.out.println("Digite as duas notas:");
-        double primeiraNota = scanner.nextDouble();
-        double segundaNota = scanner.nextDouble();
+        while(opcao != 5) {
+            System.out.println("1. Calcular media de notas.");
+            System.out.println("2. Verificar par ou impar.");
+            System.out.println("3. Converter temperatura C -> F");
+            System.out.println("4. Calculadora simples.");
+            System.out.println("5. Sair.");
 
-        double media = (primeiraNota + segundaNota) / 2;
-        String situacao;
+            Scanner scanner = new Scanner(System.in);
+            opcao = scanner.nextInt();
 
-        if (media >= 7) {
-            situacao = "APROVADO";
-        } else if (media >= 5) {
-            situacao = "RECUPERAÇÃO";
-        } else {
-            situacao = "REPROVADO";
+            double primeiroNumero;
+            double segundoNumero;
+
+            switch (opcao) {
+                case 1: {
+                    System.out.println("Digite a primeira nota");
+                    primeiroNumero = scanner.nextDouble();
+                    System.out.println("Digite a segunda nota");
+                    segundoNumero = scanner.nextDouble();
+                    double media = (primeiroNumero + segundoNumero) / 2;
+                    System.out.println("A média das notas é: " + media);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Digite o número para verificação: ");
+                    int numero = scanner.nextInt();
+
+                    String resultado = (numero%2 == 0) ? "Par" : "Ímpar";
+                    System.out.println("Esse número é: " + resultado);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Digite a temperatura em Celsius.");
+                    double temperaturaEmCelsius = scanner.nextDouble();
+                    double temperaturaFahrenheit = temperaturaEmCelsius * 1.8 + 32;
+                    System.out.println("A temperatura em Fahrenheit é: " + temperaturaFahrenheit);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Digite o primeiro número a ser calculado: ");
+                    primeiroNumero = scanner.nextDouble();
+                    System.out.println("Digite o segundo número a ser calculado: ");
+                    segundoNumero = scanner.nextDouble();
+                    System.out.println("A soma dos números é: " + (primeiroNumero + segundoNumero));
+                    System.out.println("A subtração dos números é: " + (primeiroNumero - segundoNumero));
+                    System.out.println("A divisão dos números é: " + (primeiroNumero / segundoNumero));
+                    System.out.println("A multiplicação dos números é: " + (primeiroNumero * segundoNumero));
+                    break;
+                }
+                case 5: {
+                    System.out.println("Saindo do sistema...");
+                    opcao = 5;
+                    break;
+                }
+            }
         }
-
-        System.out.println("Nome do aluno: " + nomeAluno + ", Média: " + media + ", Situação: " + situacao);
     }
-
 }
+
