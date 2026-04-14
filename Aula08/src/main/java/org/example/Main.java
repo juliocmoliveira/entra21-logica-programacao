add0b9c01e1e7701b9254b1b5fb9a27a992a6780
julio-m-oliveira_bradesco <julio.m.oliveira@bradesco.com.br>
2026-04-14 10:14:41 -0300

    feat: Atividade aula 09.

---
+++ Aula08/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroSecreto = (int) (Math.random() * 100) + 1;
+        int input;
+        int tentativas = 0;
+        int limite = 10;
+        boolean acertou = false;
+
+        System.out.println("====== JOGO DA ADIVINHAÇÃO =======");
+        System.out.println("Tentativas disponíveis: " + limite);
+        System.out.println("Insira um número de 1 a 100");
+
+        do {
+            System.out.println("Tentativa " + (tentativas + 1) + "/" + limite);
+            input = scanner.nextInt();
+            tentativas++;
+
+            if (input == numeroSecreto) {
+                acertou = true;
+                System.out.println("Parabéns! Acertou em: " + tentativas + " tentativas");
+                break;
+            } else if (input < numeroSecreto) {
+                System.out.println("O número está abaixo do correto.");
+            } else {
+                System.out.println("O número está a cima do correto.");
+            }
+
+            if (tentativas == limite) {
+                System.out.println("O número é: " + numeroSecreto);
+            }
+        }
+        while (tentativas < limite);
+    }
+}
