ca8fca7336de9a7f569624a174d17bc1d88149a5
julio-m-oliveira_bradesco <julio.m.oliveira@bradesco.com.br>
2026-04-14 19:49:58 -0300

    Atividade aula-09

--- Aula08/src/main/java/org/example/Main.java
+++ Aula08/src/main/java/org/example/Main.java
@@ -3,38 +3,60 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+
+        //Classe biblioteca do java para ler um valor do console.
         Scanner scanner = new Scanner(System.in);
+        boolean jogarDnv = true;
 
-        int numeroSecreto = (int) (Math.random() * 100) + 1;
-        int input;
-        int tentativas = 0;
-        int limite = 10;
-        boolean acertou = false;
+        while(jogarDnv) {
+            int numeroSecreto = (int) (Math.random() * 100) + 1;
+            int input;
+            int tentativas = 0;
+            int limite = 10;
 
-        System.out.println("====== JOGO DA ADIVINHAÇÃO =======");
-        System.out.println("Tentativas disponíveis: " + limite);
-        System.out.println("Insira um número de 1 a 100");
+            System.out.println("====== JOGO DA ADIVINHAÇÃO =======");
+            System.out.println("Tentativas disponíveis: " + limite);
+            System.out.println("Insira um número de 1 a 100");
 
-        do {
-            System.out.println("Tentativa " + (tentativas + 1) + "/" + limite);
-            input = scanner.nextInt();
-            tentativas++;
+            do {
+                System.out.println("Tentativa " + (tentativas + 1) + "/" + limite);
+                input = scanner.nextInt();
+                tentativas++;
 
-            if (input == numeroSecreto) {
-                acertou = true;
-                System.out.println("Parabéns! Acertou em: " + tentativas + " tentativas");
-                break;
-            } else if (input < numeroSecreto) {
-                System.out.println("O número está abaixo do correto.");
-            } else {
-                System.out.println("O número está a cima do correto.");
+                if (input == numeroSecreto) {
+                    System.out.println("Parabéns! Acertou em: " + tentativas + " tentativas");
+                    break;
+                } else if (input < numeroSecreto) {
+                    System.out.println("O número está abaixo do correto.");
+                } else {
+                    System.out.println("O número está a cima do correto.");
+                }
+
+                if (tentativas == limite) {
+                    System.out.println("O número é: " + numeroSecreto);
+                }
             }
-
-            if (tentativas == limite) {
-                System.out.println("O número é: " + numeroSecreto);
+            while (tentativas < limite);
+            System.out.println("Voce deseja jogar novamente?");
+            System.out.println("1 - SIM");
+            System.out.println("2 - NAO");
+            int opt = scanner.nextInt();
+            if(opt == 1){
+                System.out.println("COMEÇANDO O JOGO NOVAMENTE");
+                jogarDnv = true;
+            } else {
+                jogarDnv = false;
             }
         }
-        while (tentativas < limite);
+        System.out.println("FIM DE JOGO");
     }
 }
