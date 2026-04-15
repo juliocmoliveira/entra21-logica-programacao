ca534e623fc90ffb05766e7c2ae23c7612991801
julio-m-oliveira_bradesco <julio.m.oliveira@bradesco.com.br>
2026-04-14 21:41:17 -0300

    feat: Atividade desafio

--- logica/src/main/java/org/example/Main.java
+++ logica/src/main/java/org/example/Main.java
@@ -1,69 +1,30 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int opcao = 0;
-
-        while(opcao != 5) {
-            System.out.println("1. Calcular media de notas.");
-            System.out.println("2. Verificar par ou impar.");
-            System.out.println("3. Converter temperatura C -> F");
-            System.out.println("4. Calculadora simples.");
-            System.out.println("5. Sair.");
-
-            Scanner scanner = new Scanner(System.in);
-            opcao = scanner.nextInt();
-
-            double primeiroNumero;
-            double segundoNumero;
-
-            switch (opcao) {
-                case 1: {
-                    System.out.println("Digite a primeira nota");
-                    primeiroNumero = scanner.nextDouble();
-                    System.out.println("Digite a segunda nota");
-                    segundoNumero = scanner.nextDouble();
-                    double media = (primeiroNumero + segundoNumero) / 2;
-                    System.out.println("A média das notas é: " + media);
-                    break;
-                }
-                case 2: {
-                    System.out.println("Digite o número para verificação: ");
-                    int numero = scanner.nextInt();
-
-                    String resultado = (numero%2 == 0) ? "Par" : "Ímpar";
-                    System.out.println("Esse número é: " + resultado);
-                    break;
-                }
-                case 3: {
-                    System.out.println("Digite a temperatura em Celsius.");
-                    double temperaturaEmCelsius = scanner.nextDouble();
-                    double temperaturaFahrenheit = temperaturaEmCelsius * 1.8 + 32;
-                    System.out.println("A temperatura em Fahrenheit é: " + temperaturaFahrenheit);
-                    break;
-                }
-                case 4: {
-                    System.out.println("Digite o primeiro número a ser calculado: ");
-                    primeiroNumero = scanner.nextDouble();
-                    System.out.println("Digite o segundo número a ser calculado: ");
-                    segundoNumero = scanner.nextDouble();
-                    System.out.println("A soma dos números é: " + (primeiroNumero + segundoNumero));
-                    System.out.println("A subtração dos números é: " + (primeiroNumero - segundoNumero));
-                    System.out.println("A divisão dos números é: " + (primeiroNumero / segundoNumero));
-                    System.out.println("A multiplicação dos números é: " + (primeiroNumero * segundoNumero));
-                    break;
-                }
-                case 5: {
-                    System.out.println("Saindo do sistema...");
-                    opcao = 5;
-                    break;
-                }
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabuada do " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
             }
+            System.out.println();
         }
+
+        int linhas = 5;
+
+        for (int i = 1; i <= linhas; i++) {
+            for (int j = 1; j <= linhas - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
 
