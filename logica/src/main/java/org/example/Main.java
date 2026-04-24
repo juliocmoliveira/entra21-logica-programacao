4730e9a69959a62260098cdcb7ff680f9ed77104
julio-m-oliveira_bradesco <julio.m.oliveira@bradesco.com.br>
2026-04-23 21:55:20 -0300

    Alterações no main

--- logica/src/main/java/org/example/Main.java
+++ logica/src/main/java/org/example/Main.java
@@ -8,53 +8,26 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a quantidade de Alunos: ");
-        int quantidadeAlunos = scanner.nextInt();
-        scanner.nextLine();
+        int senhaCorreta = 1234;
+        int senhaLida;
+        int tentativas = 0;
 
-        String[] nomesAlunos = new String[quantidadeAlunos];
-        double[] notas1 = new double[quantidadeAlunos];
-        double[] notas2 = new double[quantidadeAlunos];
+//        do {
+//            System.out.println("Digite a senhaCorreta: ");
+//            senhaLida = scanner.nextInt();
+//        } while (senhaCorreta != senhaLida);
 
-        double mediaGeral = 0;
+        while (tentativas < 3) {
+            System.out.println("Digite a senha Correta: ");
+            senhaLida = scanner.nextInt();
 
-        for (int i = 0; i < quantidadeAlunos; i++) {
-            System.out.println("Digite o nome do aluno " + (i+1) + ": ");
-            nomesAlunos[i] = scanner.nextLine();
-            System.out.println("Digite a primeira nota do aluno: " + (i+1) + ": ");
-            notas1[i] = scanner.nextDouble();
-            System.out.println("Digite a segunda nota do aluno: " + (i+1) + ": ");
-            notas2[i] = scanner.nextDouble();
-            scanner.nextLine();
-
-            double media = (notas1[i] + notas2[i]) / 2;
-            System.out.println("A média do aluno " + nomesAlunos[i] + " é: " +  media);
-
-            if (media >= 7) {
-                System.out.println("Aluno aprovado");
-            } else {
-                System.out.println("Aluno reprovado");
+            if (senhaCorreta == senhaLida) {
+                System.out.println("Cofre aberto");
+                break;
             }
 
-            double maiorNota;
-            double menorNota;
-
-            if (notas1[i] > notas2[i]) {
-                maiorNota = notas1[i];
-                menorNota = notas2[i];
-            } else {
-                maiorNota = notas2[i];
-                menorNota = notas1[i];
-            }
-
-            System.out.println("A maior nota do aluno " + nomesAlunos[i] + " é: " + maiorNota);
-            System.out.println("A menor nota do aluno " + nomesAlunos[i] + " é: " + menorNota);
-
-            mediaGeral = mediaGeral + media;
+            tentativas++;
         }
-
-        mediaGeral = mediaGeral / quantidadeAlunos;
-        System.out.println("A média geral é: " + mediaGeral);
     }
 }
 
